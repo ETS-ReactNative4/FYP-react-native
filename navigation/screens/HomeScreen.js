6d88a4c3eb9e5f6263074acd6091d1f3cec820a1
L33tw33b <ronaldyungkuen@gmail.com>
2022-01-12 23:31:39 +0800

    Added map

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,14 +1,37 @@
-import * as React from 'react';
-import {View, Text } from 'react-native';
+import * as React from 'react'
+import {View, Text,StyleSheet,Dimensions,Image} from 'react-native';
 import { Entypo } from '@expo/vector-icons';
+import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
+const coords = require('../../constants/coords.json');
 
+const initRegion = {latitude: 22.312626,longitude: 114.222802,latitudeDelta: 0.04,longitudeDelta: 0.05};
 export default function HomeScreen({navigation}){
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
             <Entypo name="home" size={24} color="black" />
-            <Text onPress={() => alert('This is the Home Screen')} style={{ fontSize: 26, fontWeight: 'bold'}}>
-                Home Screen
-            </Text>
+            <MapView style={styles.map} initialRegion={initRegion}>
+            {
+                coords.coordinates.map(marker => (
+                    <Marker key={marker.name} coordinate={{latitude:marker.latitude, longitude:marker.longitude}} title={marker.name}>
+                    <Image source={require('../../assets/images/marker_icon.png')}/>
+                    </Marker>
+                ))
+            }
+           </MapView>  
         </View>
   );    
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
