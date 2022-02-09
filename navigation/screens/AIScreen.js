532361ac609dccd9e364fdec44dacc281bba7b62
L33tw33b <ronaldyungkuen@gmail.com>
2022-02-09 22:46:37 +0800

    Implemented custom model to AIScreen.js

--- navigation/screens/AIScreen.js
+++ navigation/screens/AIScreen.js
@@ -1,12 +1,41 @@
 import * as React from 'react';
 import {View, Text } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import {bundleResourceIO} from '@tensorflow/tfjs-react-native';
 
-export default function AIScreen({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text onPress={() => navigation.navigate('Home')} style={{ fontSize: 26, fontWeight: 'bold'}}>
-                Detail Screen
-            </Text>
-        </View>
-  );    
+export default class AIScreen extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTfReady: false,
+    };
+  }
+
+  async componentDidMount() {
+     await tf.ready()
+     this.setState({ isTfReady: true })
+
+     const modelJSON = require('../../model/model.json');
+     const modelWeights = require('../../model/weights.bin');
+     const model = await tf.loadGraphModel(bundleResourceIO(modelJSON, modelWeights));
+     this.setState({ model })
+  }
+
+
+  render() {
+    return (
+       <View style={{
+         flex: 1,
+         justifyContent: 'center',
+         alignItems: 'center'
+       }}>
+         <Text>
+           TF: {this.state.isTfReady ? "Ready" : "Waiting"}
+         </Text>
+         <Text>
+           MODEL: {this.state.model ? "Ready" : "Waiting"}
+         </Text>
+       </View>
+    )
+  }   
 }
