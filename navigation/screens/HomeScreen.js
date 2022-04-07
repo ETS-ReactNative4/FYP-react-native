f90d9787f1a3d0e633f1b8cce2108e6f8c59164e
L33tw33b <ronaldyungkuen@gmail.com>
2022-04-07 23:32:52 +0800

    Display stores using API

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,29 +1,50 @@
 
-import React,{ Component } from 'react'
+import React,{ Component,useState,useEffect } from 'react'
 import {View, Text,StyleSheet,Dimensions,Image} from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import MapView, {PROVIDER_GOOGLE, Polyline} from 'react-native-maps';
 import Marker from 'react-native-maps';
 import Ionicons  from 'react-native-vector-icons/Ionicons';
 
-const coords = require('../../constants/coords.json');
+
+export default function HomeScreen(){
 
 const initRegion = {latitude: 22.312626,longitude: 114.222802,latitudeDelta: 0.04,longitudeDelta: 0.05};
-export default class HomeScreen extends Component{
-    state = {
-    }
+const [coords,setCoords] = useState([]);
 
-    render() {
+   fetch('http://3.217.241.125/FYP_api/getStoreDetail.php', {
+       method:"POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+
+        })
+    })
+        .then((response) => response.json())
+        .then((res) => {
+
+                setCoords(res.coordinates);
+
+        })
+       .catch((error) => {
+        console.log(error); 
+        console.log(error.message);
+       })
+
+
 
     return(
+
         <View style={styles.container}>
             <Entypo name="home" size={24} color="black" />
             <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={initRegion}> 
             { //Need to specify 'MapView' before their component name e.g. <MapView.{ComponentName}>
-            coords.coordinates.map(marker => (
+            coords.map(marker => (
                 <MapView.Marker 
                 key={marker.id} 
-                coordinate={{latitude:marker.latitude, longitude:marker.longitude}} 
+                coordinate={{latitude:parseFloat(marker.latitude), longitude:parseFloat(marker.longitude)}} 
                 title={marker.name}>
                     <Image source={require("../../assets/images/marker_icon.png")}/>
                 <MapView.Callout style={styles.markerCallout}>
@@ -37,7 +58,6 @@ export default class HomeScreen extends Component{
         </View>
   )
     };    
-}
 
 const styles = StyleSheet.create({
   container: {
