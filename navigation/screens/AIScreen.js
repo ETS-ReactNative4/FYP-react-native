4b67c98eff9f9efc73cd9cdb0cbc4119c6959ee1
L33tw33b <ronaldyungkuen@gmail.com>
2022-04-08 14:54:28 +0800

    Waste counting

--- navigation/screens/AIScreen.js
+++ navigation/screens/AIScreen.js
@@ -16,6 +16,7 @@ const { width, height } = Dimensions.get('window');
 
 
 
+
 const labelMap = {
     1:{name:'Plastic', color:'brown'},
     2:{name:'Metal', color:'yellow'},
@@ -33,10 +34,12 @@ const styles = StyleSheet.create({
           height:600,
           width:400,
           top:50,
+          left:7
       },
       canvas:{
           ...StyleSheet.absoluteFillObject,
           top:50,
+          left:7,
           zIndex: 1000000000,
       },
 });
@@ -53,11 +56,16 @@ const styles = StyleSheet.create({
       width: 1600,
     };
    }
-//const NEW_OD_OUTPUT_TENSORS = ['detected_boxes', 'detected_scores', 'detected_classes'];
 
 export default function AIScreen(navigation){
+    var plastic = 0
+    var metal = 0
+    var paper= 0
+    var glass = 0; 
+
     const isFocused = useIsFocused();
     const [model, setModel] = useState();
+    const [text,setText] = useState("Plastic: "+plastic+" Metal: "+metal+" Paper: "+paper+" Glass: "+glass);
     let context = useRef(null);
     let canvas = useRef(null);
 
@@ -68,10 +76,8 @@ export default function AIScreen(navigation){
       const loop = async() => {
           const nextImageTensor = await images.next().value;
           if(nextImageTensor !== undefined && nextImageTensor !== null){
-              const resized = await tf.image.resizeBilinear(nextImageTensor.expandDims(0),[320,320]);
-              //const rotated = await tf.image.rotateWithOffset(resized,-90);
+              const resized = await tf.image.resizeBilinear(nextImageTensor.expandDims(0),[224,224]);
               const casted = await resized.cast('int32');
-              
               const output = await model.executeAsync(casted);
 
               if(!model && output) throw new Error('No model or image tensor');
@@ -90,12 +96,16 @@ export default function AIScreen(navigation){
               //}).catch((error) => {
               //  console.log(error);
               //}));
+              context.current.clearRect(0,0,width,height);
+              plastic = 0;
+              metal = 0;
+              paper = 0;
+              glass = 0;
               drawRectangle(prediction,nextImageTensor,boxes[0],classes[0],scores[0]);
               tf.dispose(nextImageTensor);
               tf.dispose(resized);
               tf.dispose(casted);
               tf.dispose(output);
-              //tf.dispose(rotated);
               tf.dispose(prediction);
               requestAnimationFrame(loop);
           } 
@@ -106,38 +116,12 @@ export default function AIScreen(navigation){
 
     function drawRectangle(prediction, nextImageTensor,boxes,classes,scores){
         if(!context.current || !canvas.current){console.log('No context/canvas'); return};
-        context.current.clearRect(0,0,width,height);
-        context.current.strokeRect(0, 0, canvas.current.width,canvas.current.height)
+        
+        //context.current.strokeRect(0, 0, canvas.current.width,canvas.current.height)
         for (let n = 0; n < boxes.length; n++) {
-                //console.log('Predicting waste');
                 if(scores[n] >= 0.8){
                     const[x,y,boxwidth,boxheight] = boxes[n];
-                        //to match the size of camera preview 
-                    //console.log(x,y,boxwidth,boxheight);
-                    const scaleWidth = canvas.current.width / nextImageTensor.shape[1]; 
-                    const scaleHeight = canvas.current.height / nextImageTensor.shape[0]; 
-
                     const flipHorizontal = Platform.OS == 'ios' ? false : true;
-
-                    //Clear previous prediction
-                    
-                    //console.log(context.current);
-                    //console.log(canvas.current);
-                     
-                    //const x = (boxes[n][0] * imageWidth) + 10;
-                    //onst y = (boxes[n][1] * imageHeight) - 10;
-                    //const width = (boxes[n][2] * imageWidth) - x + 20;
-                    //const height = (boxes[n][3] * imageHeight) - y + 10;
-
-                    //Draw the rectangle for each prediction
-                    //for(const prediction of prediction){
-                    //    const [x,y,width,height] = prediction.bbox;
-                    //}
-                    //console.log('Waste '+n+'x: '+x+'y: '+y+'width : '+width+'height : '+height);
-                    //Scale coordinates based on the ratio calculated
-                    //const boundingBoxX = flipHorizontal ? 
-                    //canvas.current.width - x*canvas.current.width - boxwidth * canvas.current.width 
-                    //: x*scaleWidth;
                     const boundingBoxX = flipHorizontal ? 
                     canvas.current.width - y*canvas.current.width - boxheight*canvas.current.width : 
                     y*canvas.current.width;
@@ -146,21 +130,25 @@ export default function AIScreen(navigation){
                     //Color each classes with different colors
                     //console.log(classes[n]);
                     //console.log(labelMap[classes[n]]['color']);
+
+                    handleNumber(labelMap[classes[n]]['name']);
+
                     context.current.strokeStyle = labelMap[classes[n]]['color'];
-                    console.log(boundingBoxX, boundingBoxY, boxheight*canvas.current.width, boxwidth*canvas.current.height);
+                    //console.log(boundingBoxX, boundingBoxY, boxheight*canvas.current.width, boxwidth*canvas.current.height);
                     context.current.strokeRect(boundingBoxX, boundingBoxY, boxheight*canvas.current.width, boxwidth*canvas.current.height);
 
                     context.current.fillText(labelMap[classes[n]]['name'],boundingBoxX - 5,boundingBoxY - 5);
                 }
+            
             }
-
+    setText("Plastic: "+plastic+" Metal: "+metal+" Paper: "+paper+" Glass: "+glass);
     }
 
    async function handleCanvas(can){ 
        if(can){
            can.width = 400;
            can.height = 600;
-           console.log(can.width,can.height);
+           //console.log(can.width,can.height);
            const ctx = can.getContext('2d');
            ctx.fillStyle = 'white';
            ctx.lineWidth = 3;
@@ -169,6 +157,21 @@ export default function AIScreen(navigation){
            canvas.current = can;
        }
    }
+  function handleNumber(type){ 
+    if(type == 'Plastic'){
+        plastic = plastic+1;
+    }else if(type == 'Metal'){
+        metal = metal+1;
+    }else if(type == 'Paper'){
+        paper = paper + 1;
+    }else if(type == 'Glass'){
+        glass = glass + 1;
+    }
+  }
+
+  function changeText(paramtext){
+    this.setState()
+  } 
 
   useEffect(() => {
       async function readyCamera(){
@@ -188,18 +191,19 @@ export default function AIScreen(navigation){
 
     return (
 
-       <View style={{
+                   <View style={{
          flex: 1,
          justifyContent: 'center',
          alignItems: 'center'
        }}>
+            <Text style={{bottom:330}} >{text}</Text> 
         {   isFocused && //Mount camera once loaded in AIScreen and unmount when not in AIScreen
             <TensorCamera style={styles.camera}
             type={Camera.Constants.Type.back}
             cameraTextureHeight={textureDims.height}
             cameraTextureWidth={textureDims.width}
-            resizeWidth={320}
-            resizeHeight={320}
+            resizeWidth={224}
+            resizeHeight={224}
             resizeDepth={3}
             onReady={handleCameraStream}
             autorender={true}
